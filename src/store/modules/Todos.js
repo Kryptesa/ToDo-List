72d12f84c26ab861e1fe5fae8510d97e1c808997
KirillMorozov <kirill.morozov@rubius.com>
2019-12-22 14:24:02 +0700

    Test

--- src/store/modules/Todos.js
+++ src/store/modules/Todos.js
@@ -1,4 +1,4 @@
-import Axios from 'axios';
+import axios from 'axios';
 
 /* eslint no-shadow: ["error", { "allow": ["state"] }] */
 
@@ -12,22 +12,22 @@ const getters = {
 
 const actions = {
   async fetchTodos({ commit }) {
-    const response = await Axios.get('https://jsonplaceholder.typicode.com/todos');
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
 
     commit('setTodos', response.data);
   },
   async addTodo({ commit }, title) {
-    const response = await Axios.post('https://jsonplaceholder.typicode.com/todos', { title, completed: false });
+    const response = await axios.post('https://jsonplaceholder.typicode.com/todos', { title, completed: false });
 
     commit('newTodo', response.data);
   },
   async deleteTodo({ commit }, id) {
-    await Axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
 
     commit('deleteTodo', id);
   },
   async updateTodo({ commit }, updTodo) {
-    await Axios.put(`https://jsonplaceholder.typicode.com/todos/${updTodo.id}`, updTodo);
+    await axios.put(`https://jsonplaceholder.typicode.com/todos/${updTodo.id}`, updTodo);
 
     commit('updateTodo', updTodo);
   },
